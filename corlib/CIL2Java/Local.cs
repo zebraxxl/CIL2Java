83911474070ccb5410d1b0478172aca3377d3eb7
zebraxxl <zebraxxl@mail.ru>
2014-11-24 17:05:22 +1200

    In corlib CIL2Java.Local changed to Local

--- corlib/CIL2Java/Local.cs
+++ corlib/CIL2Java/Local.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace CIL2Java
-{
-    public static class Local
-    {
-        public static string GetText(string Text)
-        {
-            return Text;
-        }
 
-        public static string GetText(string Text, params object[] values)
-        {
-            return string.Format(Text, values);
-        }
+public static class Local
+{
+    public static string GetText(string Text)
+    {
+        return Text;
+    }
+
+    public static string GetText(string Text, params object[] values)
+    {
+        return string.Format(Text, values);
     }
 }
+
