4c366d214d8995324bb9e8a3acc2acc09847b804
zebraxxl <zebraxxl@mail.ru>
2014-11-30 21:46:32 +1200

    CodeCompiler::GenerateMethodProlog moved to CodeCompiler.Prolog.cs

---
+++ CIL2Java/CodeCompiler/CodeCompiler.Prolog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CIL2Java
+{
+    public partial class CodeCompiler
+    {
+        private byte[] GenerateMethodProlog()
+        {
+            JavaBytecodeWriter codeWriter = new JavaBytecodeWriter();
+
+            foreach (ValueTypeVar v in valueTypesVars)
+            {
+                Java.Constants.Class typeRef = new Java.Constants.Class(namesController.TypeNameToJava(v.varType.Fullname));
+                Java.Constants.MethodRef ctorRef = new Java.Constants.MethodRef(typeRef.Value,
+                    ClassNames.JavaConstructorMethodName, "()V");
+
+                codeWriter
+                    .Add(Java.OpCodes._new, typeRef)
+                    .Add(Java.OpCodes.dup)
+                    .Add(Java.OpCodes.invokespecial, ctorRef)
+                    .AddStore(JavaPrimitiveType.Ref, v.varIndex);
+            }
+
+            return codeWriter.End(constsPool).CodeBytes;
+        }
+    }
+}
--- CIL2Java/CodeCompiler/CodeCompiler.Vars.cs
+++ CIL2Java/CodeCompiler/CodeCompiler.Vars.cs
@@ -133,25 +133,5 @@ namespace CIL2Java
             if ((type == JavaPrimitiveType.Double) || (type == JavaPrimitiveType.Long))
                 freeVars.Add(varType + 1);
         }
-
-        private byte[] GenerateMethodProlog()
-        {
-            JavaBytecodeWriter codeWriter = new JavaBytecodeWriter();
-
-            foreach (ValueTypeVar v in valueTypesVars)
-            {
-                Java.Constants.Class typeRef = new Java.Constants.Class(namesController.TypeNameToJava(v.varType.Fullname));
-                Java.Constants.MethodRef ctorRef = new Java.Constants.MethodRef(typeRef.Value,
-                    ClassNames.JavaConstructorMethodName, "()V");
-
-                codeWriter
-                    .Add(Java.OpCodes._new, typeRef)
-                    .Add(Java.OpCodes.dup)
-                    .Add(Java.OpCodes.invokespecial, ctorRef)
-                    .AddStore(JavaPrimitiveType.Ref, v.varIndex);
-            }
-
-            return codeWriter.End(constsPool).CodeBytes;
-        }
     }
 }
