cb8619e04f743a69c2f68ce17c9c0177a309b71a
zebraxxl <zebraxxl@mail.ru>
2014-11-30 23:16:12 +1200

    Preprocessor find default ctor call in value type ctor

--- CIL2Java/CodeCompiler/CodeCompiler.Preprocessor.cs
+++ CIL2Java/CodeCompiler/CodeCompiler.Preprocessor.cs
@@ -7,6 +7,7 @@ namespace CIL2Java
 {
     public partial class CodeCompiler
     {
+        //Find this() or super() calls in ctor to move it to begin of method
         private void PreprocessorFindAndMoveSuperOrThisCalls()
         {
             if (!thisMethod.IsConstructor) return;
@@ -37,8 +38,30 @@ namespace CIL2Java
             }
         }
 
+        //Find stobj(ldloc(this), defaultValue()). It is equals to this()
+        private void PreprocessorFindDefaultCtorCallInValueTypeCtor()
+        {
+            if ((!thisMethod.IsConstructor) || (!thisMethod.DeclaringType.IsValueType)) return;
+
+            foreach (ILNode node in ilBody.Body)
+            {
+                ILExpression e = node as ILExpression;
+                if (e == null) continue;
+
+                if (e.Code != ILCode.Stobj) continue;
+                if (e.Arguments[0].Code != ILCode.Ldloc) continue;
+                if (((ILVariable)e.Arguments[0].Operand).Name != "this") continue;
+                if (e.Arguments[1].Code != ILCode.DefaultValue) continue;
+
+                //Founded signature. Just remove it. Prolog generator will add call to super() or this()
+                ilBody.Body.Remove(e);
+                break;
+            }
+        }
+
         private void RunPreprocessor()
         {
+            PreprocessorFindDefaultCtorCallInValueTypeCtor();
             PreprocessorFindAndMoveSuperOrThisCalls();
         }
     }
