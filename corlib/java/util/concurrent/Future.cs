f38930ba1fa76ea7b00183de9eccb4ba8c4112f8
zebraxxl <zebraxxl@mail.ru>
2014-12-05 23:14:45 +1200

    java.util.concurrent.Future

---
+++ corlib/java/util/concurrent/Future.cs
@@ -0,0 +1,16 @@
+﻿using CIL2Java.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace java.util.concurrent
+{
+    [FromJava]
+    public interface Future
+    {
+        [FromJava] bool cancel(bool mayInterruptIfRunning);
+        [FromJava] bool isCancelled();
+        [FromJava] bool isDone();
+        [FromJava] object get();
+        [FromJava] object get(long timeout, TimeUnit unit);
+    }
+}
