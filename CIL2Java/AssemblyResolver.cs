01beb31e5b45754da1f0b30e227ffad99b6d2564
zebraxxl <zebraxxl@mail.ru>
2014-12-09 23:44:11 +1200

    Assembly reading not founded debug  symbols bug fixed

--- CIL2Java/AssemblyResolver.cs
+++ CIL2Java/AssemblyResolver.cs
@@ -12,10 +12,30 @@ namespace CIL2Java
 
             if (Program.CachedAssemblies.ContainsKey(name.Name))
                 return Program.CachedAssemblies[name.Name];
-            if (Program.ReplacedAssemblies.ContainsKey(name.Name))
-                result = base.Resolve(Program.ReplacedAssemblies[name.Name], Program.AssemblyReaderParams);
-            else
-                result = base.Resolve(name, Program.AssemblyReaderParams);
+
+            try
+            {
+                if (Program.ReplacedAssemblies.ContainsKey(name.Name))
+                    result = base.Resolve(Program.ReplacedAssemblies[name.Name], Program.AssemblyReaderParams);
+                else
+                    result = base.Resolve(name, Program.AssemblyReaderParams);
+            }
+            catch (Exception)
+            {
+                if (Program.AssemblyReaderParams.ReadSymbols)
+                {
+                    Program.AssemblyReaderParams.ReadSymbols = false;
+
+                    if (Program.ReplacedAssemblies.ContainsKey(name.Name))
+                        result = base.Resolve(Program.ReplacedAssemblies[name.Name], Program.AssemblyReaderParams);
+                    else
+                        result = base.Resolve(name, Program.AssemblyReaderParams);
+
+                    Program.AssemblyReaderParams.ReadSymbols = true;
+                }
+                else
+                    throw;
+            }
 
             Program.CachedAssemblies.Add(name.Name, result);
 
