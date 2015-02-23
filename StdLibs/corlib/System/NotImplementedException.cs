3b3b75ba6e088102891b10651d9052d210313584
zebraxxl <zebraxxl@mail.ru>
2015-02-23 15:29:51 +0300

    corlib: System.NotImplementedException implement

--- StdLibs/corlib/System/NotImplementedException.cs
+++ StdLibs/corlib/System/NotImplementedException.cs
@@ -9,30 +9,33 @@ namespace System
     public class NotImplementedException : SystemException
     {
         public NotImplementedException()
+            : base(Local.GetText("The method or operation is not implemented."))
         {
+            HResult = __HResults.E_NOTIMPL;
         }
         
         
         /// <summary>Initializes a new instance of the <see cref="T:System.NotImplementedException" /> class with a specified error message.</summary><param name="message">The error message that explains the reason for the exception. </param>
         public NotImplementedException(string message)
+            : base(message)
         {
-             throw new NotImplementedException();
+            HResult = __HResults.E_NOTIMPL;
         }
         
         
         /// <summary>Initializes a new instance of the <see cref="T:System.NotImplementedException" /> class with a specified error message and a reference to the inner exception that is the cause of this exception.</summary><param name="message">The error message that explains the reason for the exception. </param><param name="inner">The exception that is the cause of the current exception. If the <paramref name="inner" /> parameter is not null, the current exception is raised in a catch block that handles the inner exception. </param>
         public NotImplementedException(string message, Exception inner)
+            : base(message, inner)
         {
-             throw new NotImplementedException();
+            HResult = __HResults.E_NOTIMPL;
         }
         
         
         /// <summary>Initializes a new instance of the <see cref="T:System.NotImplementedException" /> class with serialized data.</summary><param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo" /> that holds the serialized object data about the exception being thrown. </param><param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext" /> that contains contextual information about the source or destination. </param>
         protected NotImplementedException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
-             throw new NotImplementedException();
+            HResult = __HResults.E_NOTIMPL;
         }
-        
-        
     }
 }
