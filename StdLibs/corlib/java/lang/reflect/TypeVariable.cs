85e3282bff0df962d211e7fd15c9868492fe91ab
zebraxxl <zebraxxl@mail.ru>
2015-03-16 00:52:45 +0300

    java.lang.reflect.TypeVariable added

---
+++ StdLibs/corlib/java/lang/reflect/TypeVariable.cs
@@ -0,0 +1,14 @@
+﻿using CIL2Java.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace java.lang.reflect
+{
+    [FromJava]
+    public interface TypeVariable : Type
+    {
+        [FromJava] Type[] getBounds();
+        [FromJava] object getGenericDeclaration();
+        [FromJava] string getName();
+    }
+}
