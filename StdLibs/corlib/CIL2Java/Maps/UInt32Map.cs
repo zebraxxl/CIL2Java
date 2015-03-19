2c0aac2da3320e0c711f67845c7ad1cda8932891
zebraxxl <zebraxxl@mail.ru>
2015-03-19 23:55:55 +0300

    corlib: System.UInt32

---
+++ StdLibs/corlib/CIL2Java/Maps/UInt32Map.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using javaInt = java.lang.Integer;
+
+namespace CIL2Java.Maps
+{
+    public class UInt32Map : IComparable, IFormattable, IConvertible, IComparable<UInt32>, IEquatable<UInt32>
+    {
+        private uint m_value;
+
+        private UInt32Map(uint value)
+        {
+            this.m_value = value;
+        }
+
+        public static UInt32Map GetAdapter(javaInt self)
+        {
+            return new UInt32Map(unchecked((uint)self.intValue()));
+        }
+
+        #region Mapped methods
+        public static int CompareTo(javaInt self, object value)
+        {
+            uint m_value = unchecked((uint)self.intValue());
+
+            if (value == null)
+            {
+                return 1;
+            }
+            if (value is UInt32)
+            {
+                // Need to use compare because subtraction will wrap
+                // to positive for very large neg numbers, etc.
+                uint i = (uint)value;
+                if (m_value < i) return -1;
+                if (m_value > i) return 1;
+                return 0;
+            }
+            throw new ArgumentException(Environment.GetResourceString("Arg_MustBeUInt32"));
+        }
+
+        public static int CompareTo(javaInt self, uint value)
+        {
+            uint m_value = unchecked((uint)self.intValue());
+
+            // Need to use compare because subtraction will wrap
+            // to positive for very large neg numbers, etc.
+            if (m_value < value) return -1;
+            if (m_value > value) return 1;
+            return 0;
+        }
+
+        public static bool Equals(javaInt self, object obj)
+        {
+            uint m_value = unchecked((uint)self.intValue());
+
+            if (!(obj is UInt32))
+            {
+                return false;
+            }
+            return m_value == ((uint)obj);
+        }
+
+        public static bool Equals(javaInt self, uint obj)
+        {
+            uint m_value = unchecked((uint)self.intValue());
+
+            return m_value == obj;
+        }
+
+        public static string ToString(javaInt self)
+        {
+            uint m_value = unchecked((uint)self.intValue());
+
+            return Number.FormatUInt32(m_value, null, NumberFormatInfo.CurrentInfo);
+        }
+
+        public static string ToString(javaInt self, IFormatProvider provider)
+        {
+            uint m_value = unchecked((uint)self.intValue());
+
+            return Number.FormatUInt32(m_value, null, NumberFormatInfo.GetInstance(provider));
+        }
+
+        public static string ToString(javaInt self, string format)
+        {
+            uint m_value = unchecked((uint)self.intValue());
+
+            return Number.FormatUInt32(m_value, format, NumberFormatInfo.CurrentInfo);
+        }
+
+        public static string ToString(javaInt self, string format, IFormatProvider provider)
+        {
+            uint m_value = unchecked((uint)self.intValue());
+
+            return Number.FormatUInt32(m_value, format, NumberFormatInfo.GetInstance(provider));
+        }
+
+        public static uint Parse(string s)
+        {
+            return Number.ParseUInt32(s, NumberStyles.Integer, NumberFormatInfo.CurrentInfo);
+        }
+
+        public static uint Parse(string s, NumberStyles style)
+        {
+            NumberFormatInfo.ValidateParseStyleInteger(style);
+            return Number.ParseUInt32(s, style, NumberFormatInfo.CurrentInfo);
+        }
+
+        public static uint Parse(string s, IFormatProvider provider)
+        {
+            return Number.ParseUInt32(s, NumberStyles.Integer, NumberFormatInfo.GetInstance(provider));
+        }
+
+        public static uint Parse(string s, NumberStyles style, IFormatProvider provider)
+        {
+            NumberFormatInfo.ValidateParseStyleInteger(style);
+            return Number.ParseUInt32(s, style, NumberFormatInfo.GetInstance(provider));
+        }
+
+        public static bool TryParse(string s, out uint result)
+        {
+            return Number.TryParseUInt32(s, NumberStyles.Integer, NumberFormatInfo.CurrentInfo, out result);
+        }
+
+        public static bool TryParse(string s, NumberStyles style, IFormatProvider provider, out uint result)
+        {
+            NumberFormatInfo.ValidateParseStyleInteger(style);
+            return Number.TryParseUInt32(s, style, NumberFormatInfo.GetInstance(provider), out result);
+        }
+
+        public static TypeCode GetTypeCode(javaInt self)
+        {
+            return TypeCode.UInt32;
+        }
+        #endregion
+
+        public int CompareTo(object value)
+        {
+            if (value == null)
+            {
+                return 1;
+            }
+            if (value is UInt32)
+            {
+                // Need to use compare because subtraction will wrap
+                // to positive for very large neg numbers, etc.
+                uint i = (uint)value;
+                if (m_value < i) return -1;
+                if (m_value > i) return 1;
+                return 0;
+            }
+            throw new ArgumentException(Environment.GetResourceString("Arg_MustBeUInt32"));
+        }
+
+        public int CompareTo(uint value)
+        {
+            // Need to use compare because subtraction will wrap
+            // to positive for very large neg numbers, etc.
+            if (m_value < value) return -1;
+            if (m_value > value) return 1;
+            return 0;
+        }
+
+        public bool Equals(uint other)
+        {
+            return m_value == other;
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return m_value.ToString(format, formatProvider);
+        }
+
+        public TypeCode GetTypeCode()
+        {
+            return TypeCode.UInt32;
+        }
+
+        /// <internalonly/>
+        public bool ToBoolean(IFormatProvider provider)
+        {
+            return Convert.ToBoolean(m_value);
+        }
+
+        /// <internalonly/>
+        public char ToChar(IFormatProvider provider)
+        {
+            return Convert.ToChar(m_value);
+        }
+
+        /// <internalonly/>
+        public sbyte ToSByte(IFormatProvider provider)
+        {
+            return Convert.ToSByte(m_value);
+        }
+
+        /// <internalonly/>
+        public byte ToByte(IFormatProvider provider)
+        {
+            return Convert.ToByte(m_value);
+        }
+
+        /// <internalonly/>
+        public short ToInt16(IFormatProvider provider)
+        {
+            return Convert.ToInt16(m_value);
+        }
+
+        /// <internalonly/>
+        public ushort ToUInt16(IFormatProvider provider)
+        {
+            return Convert.ToUInt16(m_value);
+        }
+
+        /// <internalonly/>
+        public int ToInt32(IFormatProvider provider)
+        {
+            return Convert.ToInt32(m_value);
+        }
+
+        /// <internalonly/>
+        public uint ToUInt32(IFormatProvider provider)
+        {
+            return m_value;
+        }
+
+        /// <internalonly/>
+        public long ToInt64(IFormatProvider provider)
+        {
+            return Convert.ToInt64(m_value);
+        }
+
+        /// <internalonly/>
+        public ulong ToUInt64(IFormatProvider provider)
+        {
+            return Convert.ToUInt64(m_value);
+        }
+
+        /// <internalonly/>
+        public float ToSingle(IFormatProvider provider)
+        {
+            return Convert.ToSingle(m_value);
+        }
+
+        /// <internalonly/>
+        public double ToDouble(IFormatProvider provider)
+        {
+            return Convert.ToDouble(m_value);
+        }
+
+        /// <internalonly/>
+        public Decimal ToDecimal(IFormatProvider provider)
+        {
+            return Convert.ToDecimal(m_value);
+        }
+
+        /// <internalonly/>
+        public DateTime ToDateTime(IFormatProvider provider)
+        {
+            throw new InvalidCastException(Environment.GetResourceString("InvalidCast_FromTo", "UInt32", "DateTime"));
+        }
+
+        public string ToString(IFormatProvider provider)
+        {
+            return m_value.ToString(provider);
+        }
+
+        public object ToType(Type conversionType, IFormatProvider provider)
+        {
+            //TODO: UInt32 - ToType
+            throw new NotImplementedException();
+        }
+    }
+}
--- StdLibs/corlib/System/UInt32.cs
+++ StdLibs/corlib/System/UInt32.cs
@@ -2,12 +2,16 @@ using System.Runtime.InteropServices;
 using System.Security;
 using System.Globalization;
 using CIL2Java.Attributes;
+using javaInt = java.lang.Integer;
+using mapUInt = CIL2Java.Maps.UInt32Map;
 
 namespace System
 {
     /// <summary>Represents a 32-bit unsigned integer.</summary><filterpriority>1</filterpriority>
     [Serializable]
     [ComVisibleAttribute(true)]
+    [JavaBoxTypeMap(typeof(javaInt))]
+    [JavaBoxedInterfacesMap(typeof(mapUInt))]
     public struct UInt32 : IComparable, IFormattable, IConvertible, IComparable<UInt32>, IEquatable<UInt32>
     {
         /// <summary>Represents the largest possible value of <see cref="T:System.UInt32" />. This field is constant.</summary><filterpriority>1</filterpriority>
@@ -15,11 +19,11 @@ namespace System
         /// <summary>Represents the smallest possible value of <see cref="T:System.UInt32" />. This field is constant.</summary><filterpriority>1</filterpriority>
         public const uint MinValue = 0;
 
-        private uint value;
+        private uint m_value;
 
         private UInt32(uint value)
         {
-            this.value = value;
+            this.m_value = value;
         }
 
         [return: Boxed]
@@ -29,199 +33,236 @@ namespace System
         }
     
         /// <summary>Compares this instance to a specified object and returns an indication of their relative values.</summary><returns>A signed number indicating the relative values of this instance and <paramref name="value" />.Return Value Description Less than zero This instance is less than <paramref name="value" />. Zero This instance is equal to <paramref name="value" />. Greater than zero This instance is greater than <paramref name="value" />.-or- <paramref name="value" /> is null. </returns><param name="value">An object to compare, or null. </param><exception cref="T:System.ArgumentException"><paramref name="value" /> is not a <see cref="T:System.UInt32" />. </exception><filterpriority>2</filterpriority>
+        [JavaBoxMethodMap(typeof(mapUInt), "CompareTo", true)]
         public int CompareTo(object value)
         {
-             throw new NotImplementedException();
+            if (value == null)
+            {
+                return 1;
+            }
+            if (value is UInt32)
+            {
+                // Need to use compare because subtraction will wrap
+                // to positive for very large neg numbers, etc.
+                uint i = (uint)value;
+                if (m_value < i) return -1;
+                if (m_value > i) return 1;
+                return 0;
+            }
+            throw new ArgumentException(Environment.GetResourceString("Arg_MustBeUInt32"));
         }
-        
         
         /// <summary>Compares this instance to a specified 32-bit unsigned integer and returns an indication of their relative values.</summary><returns>A signed number indicating the relative values of this instance and <paramref name="value" />.Return value Description Less than zero This instance is less than <paramref name="value" />. Zero This instance is equal to <paramref name="value" />. Greater than zero This instance is greater than <paramref name="value" />. </returns><param name="value">An unsigned integer to compare. </param><filterpriority>2</filterpriority>
+        [JavaBoxMethodMap(typeof(mapUInt), "CompareTo", true)]
         public int CompareTo(uint value)
         {
-             throw new NotImplementedException();
+            // Need to use compare because subtraction will wrap
+            // to positive for very large neg numbers, etc.
+            if (m_value < value) return -1;
+            if (m_value > value) return 1;
+            return 0;
         }
-        
         
         /// <summary>Returns a value indicating whether this instance is equal to a specified object.</summary><returns>true if <paramref name="obj" /> is an instance of <see cref="T:System.UInt32" /> and equals the value of this instance; otherwise, false.</returns><param name="obj">An object to compare with this instance. </param><filterpriority>2</filterpriority>
+        [JavaBoxMethodMap(typeof(mapUInt), "equals", true)]
         public override bool Equals(object obj)
         {
-             throw new NotImplementedException();
+            if (!(obj is UInt32))
+            {
+                return false;
+            }
+            return m_value == ((UInt32)obj).m_value;
         }
-        
         
         /// <summary>Returns a value indicating whether this instance is equal to a specified <see cref="T:System.UInt32" />.</summary><returns>true if <paramref name="obj" /> has the same value as this instance; otherwise, false.</returns><param name="obj">A value to compare to this instance.</param><filterpriority>2</filterpriority>
+        [JavaBoxMethodMap(typeof(mapUInt), "equals", true)]
         public bool Equals(uint obj)
         {
-             throw new NotImplementedException();
+            return m_value == obj;
         }
         
-        
+        [JavaBoxMethodMap(typeof(javaInt), "hashCode")]
         public override int GetHashCode()
         {
-             throw new NotImplementedException();
+            return ((int)m_value);
         }
-        
         
         [SecuritySafeCriticalAttribute()]
+        [JavaBoxMethodMap(typeof(mapUInt), "toString", true)]
         public override string ToString()
         {
-            return java.lang.Integer.toHexString((int)this.value);
+            return Number.FormatUInt32(m_value, null, NumberFormatInfo.CurrentInfo);
         }
-        
         
         /// <summary>Converts the numeric value of this instance to its equivalent string representation using the specified culture-specific format information.</summary><returns>The string representation of the value of this instance, which consists of a sequence of digits ranging from 0 to 9, without a sign or leading zeros.</returns><param name="provider">An object that supplies culture-specific formatting information. </param><filterpriority>1</filterpriority>
         [SecuritySafeCriticalAttribute()]
+        [JavaBoxMethodMap(typeof(mapUInt), "toString", true)]
         public string ToString(IFormatProvider provider)
         {
-             throw new NotImplementedException();
+            return Number.FormatUInt32(m_value, null, NumberFormatInfo.GetInstance(provider));
         }
-        
         
         /// <summary>Converts the numeric value of this instance to its equivalent string representation using the specified format.</summary><returns>The string representation of the value of this instance as specified by <paramref name="format" />.</returns><param name="format">A numeric format string.</param><exception cref="T:System.FormatException">The <paramref name="format" /> parameter is invalid. </exception><filterpriority>1</filterpriority>
         [SecuritySafeCriticalAttribute()]
+        [JavaBoxMethodMap(typeof(mapUInt), "toString", true)]
         public string ToString(string format)
         {
-             throw new NotImplementedException();
+            return Number.FormatUInt32(m_value, format, NumberFormatInfo.CurrentInfo);
         }
-        
         
         /// <summary>Converts the numeric value of this instance to its equivalent string representation using the specified format and culture-specific format information.</summary><returns>The string representation of the value of this instance as specified by <paramref name="format" /> and <paramref name="provider" />.</returns><param name="format">A numeric format string.</param><param name="provider">An object that supplies culture-specific formatting information about this instance. </param><exception cref="T:System.FormatException">The <paramref name="format" /> parameter is invalid. </exception><filterpriority>1</filterpriority>
         [SecuritySafeCriticalAttribute()]
+        [JavaBoxMethodMap(typeof(mapUInt), "toString", true)]
         public string ToString(string format, IFormatProvider provider)
         {
-             throw new NotImplementedException();
+            return Number.FormatUInt32(m_value, format, NumberFormatInfo.GetInstance(provider));
         }
         
         
         /// <summary>Converts the string representation of a number to its 32-bit unsigned integer equivalent.</summary><returns>A 32-bit unsigned integer equivalent to the number contained in <paramref name="s" />.</returns><param name="s">A string representing the number to convert. </param><exception cref="T:System.ArgumentNullException">The <paramref name="s" /> parameter is null. </exception><exception cref="T:System.FormatException">The <paramref name="s" /> parameter is not of the correct format. </exception><exception cref="T:System.OverflowException">The <paramref name="s" /> parameter represents a number that is less than <see cref="F:System.UInt32.MinValue" /> or greater than <see cref="F:System.UInt32.MaxValue" />. </exception><filterpriority>1</filterpriority>
+        [JavaBoxMethodMap(typeof(mapUInt), "Parse")]
         public static uint Parse(string s)
         {
-             throw new NotImplementedException();
+            return Number.ParseUInt32(s, NumberStyles.Integer, NumberFormatInfo.CurrentInfo);
         }
-        
         
         /// <summary>Converts the string representation of a number in a specified style to its 32-bit unsigned integer equivalent.</summary><returns>A 32-bit unsigned integer equivalent to the number specified in <paramref name="s" />.</returns><param name="s">A string representing the number to convert. The string is interpreted by using the style specified by the <paramref name="style" /> parameter.</param><param name="style">A bitwise combination of the enumeration values that specify the permitted format of <paramref name="s" />. A typical value to specify is <see cref="F:System.Globalization.NumberStyles.Integer" />.</param><exception cref="T:System.ArgumentNullException"><paramref name="s" /> is null. </exception><exception cref="T:System.ArgumentException"><paramref name="style" /> is not a <see cref="T:System.Globalization.NumberStyles" /> value. -or-<paramref name="style" /> is not a combination of <see cref="F:System.Globalization.NumberStyles.AllowHexSpecifier" /> and <see cref="F:System.Globalization.NumberStyles.HexNumber" /> values.</exception><exception cref="T:System.FormatException"><paramref name="s" /> is not in a format compliant with <paramref name="style" />. </exception><exception cref="T:System.OverflowException"><paramref name="s" /> represents a number that is less than <see cref="F:System.UInt32.MinValue" /> or greater than <see cref="F:System.UInt32.MaxValue" />. -or-<paramref name="s" /> includes non-zero, fractional digits.</exception><filterpriority>1</filterpriority>
+        [JavaBoxMethodMap(typeof(mapUInt), "Parse")]
         public static uint Parse(string s, NumberStyles style)
         {
-             throw new NotImplementedException();
+            NumberFormatInfo.ValidateParseStyleInteger(style);
+            return Number.ParseUInt32(s, style, NumberFormatInfo.CurrentInfo);
         }
-        
         
         /// <summary>Converts the string representation of a number in a specified culture-specific format to its 32-bit unsigned integer equivalent.</summary><returns>A 32-bit unsigned integer equivalent to the number specified in <paramref name="s" />.</returns><param name="s">A string that represents the number to convert. </param><param name="provider">An object that supplies culture-specific formatting information about <paramref name="s" />. </param><exception cref="T:System.ArgumentNullException"><paramref name="s" /> is null. </exception><exception cref="T:System.FormatException"><paramref name="s" /> is not in the correct style. </exception><exception cref="T:System.OverflowException"><paramref name="s" /> represents a number that is less than <see cref="F:System.UInt32.MinValue" /> or greater than <see cref="F:System.UInt32.MaxValue" />. </exception><filterpriority>1</filterpriority>
+        [JavaBoxMethodMap(typeof(mapUInt), "Parse")]
         public static uint Parse(string s, IFormatProvider provider)
         {
-             throw new NotImplementedException();
+            return Number.ParseUInt32(s, NumberStyles.Integer, NumberFormatInfo.GetInstance(provider));
         }
-        
         
         /// <summary>Converts the string representation of a number in a specified style and culture-specific format to its 32-bit unsigned integer equivalent.</summary><returns>A 32-bit unsigned integer equivalent to the number specified in <paramref name="s" />.</returns><param name="s">A string representing the number to convert. The string is interpreted by using the style specified by the <paramref name="style" /> parameter.</param><param name="style">A bitwise combination of enumeration values that indicates the style elements that can be present in <paramref name="s" />. A typical value to specify is <see cref="F:System.Globalization.NumberStyles.Integer" />.</param><param name="provider">An object that supplies culture-specific formatting information about <paramref name="s" />. </param><exception cref="T:System.ArgumentNullException"><paramref name="s" /> is null. </exception><exception cref="T:System.ArgumentException"><paramref name="style" /> is not a <see cref="T:System.Globalization.NumberStyles" /> value. -or-<paramref name="style" /> is not a combination of <see cref="F:System.Globalization.NumberStyles.AllowHexSpecifier" /> and <see cref="F:System.Globalization.NumberStyles.HexNumber" /> values.</exception><exception cref="T:System.FormatException"><paramref name="s" /> is not in a format compliant with <paramref name="style" />. </exception><exception cref="T:System.OverflowException"><paramref name="s" /> represents a number that is less than <see cref="F:System.UInt32.MinValue" /> or greater than <see cref="F:System.UInt32.MaxValue" />. -or-<paramref name="s" /> includes non-zero, fractional digits.</exception><filterpriority>1</filterpriority>
+        [JavaBoxMethodMap(typeof(mapUInt), "Parse")]
         public static uint Parse(string s, NumberStyles style, IFormatProvider provider)
         {
-             throw new NotImplementedException();
-        }
-        
-        
-        public static bool TryParse(string s, ref uint result)
-        {
-             throw new NotImplementedException();
-        }
-        
-        
-        public static bool TryParse(string s, NumberStyles style, IFormatProvider provider, ref uint result)
-        {
-             throw new NotImplementedException();
-        }
-        
-        
-        public TypeCode GetTypeCode()
-        {
-             throw new NotImplementedException();
+            NumberFormatInfo.ValidateParseStyleInteger(style);
+            return Number.ParseUInt32(s, style, NumberFormatInfo.GetInstance(provider));
         }
 
+
+        [JavaBoxMethodMap(typeof(mapUInt), "TryParse")]
+        public static bool TryParse(string s, out uint result)
+        {
+            return Number.TryParseUInt32(s, NumberStyles.Integer, NumberFormatInfo.CurrentInfo, out result);
+        }
+
+        [JavaBoxMethodMap(typeof(mapUInt), "TryParse")]
+        public static bool TryParse(string s, NumberStyles style, IFormatProvider provider, out uint result)
+        {
+            NumberFormatInfo.ValidateParseStyleInteger(style);
+            return Number.TryParseUInt32(s, style, NumberFormatInfo.GetInstance(provider), out result);
+        }
+
+
+        [JavaBoxMethodMap(typeof(mapUInt), "GetTypeCode", true)]
+        public TypeCode GetTypeCode()
+        {
+            return TypeCode.UInt32;
+        }
 
 
         TypeCode IConvertible.GetTypeCode()
         {
-            throw new NotImplementedException();
+            return TypeCode.UInt32;
         }
 
+        /// <internalonly/>
         bool IConvertible.ToBoolean(IFormatProvider provider)
         {
-            throw new NotImplementedException();
+            return Convert.ToBoolean(m_value);
         }
 
+        /// <internalonly/>
         char IConvertible.ToChar(IFormatProvider provider)
         {
-            throw new NotImplementedException();
+            return Convert.ToChar(m_value);
         }
 
+        /// <internalonly/>
         sbyte IConvertible.ToSByte(IFormatProvider provider)
         {
-            throw new NotImplementedException();
+            return Convert.ToSByte(m_value);
         }
 
+        /// <internalonly/>
         byte IConvertible.ToByte(IFormatProvider provider)
         {
-            throw new NotImplementedException();
+            return Convert.ToByte(m_value);
         }
 
+        /// <internalonly/>
         short IConvertible.ToInt16(IFormatProvider provider)
         {
-            throw new NotImplementedException();
+            return Convert.ToInt16(m_value);
         }
 
+        /// <internalonly/>
         ushort IConvertible.ToUInt16(IFormatProvider provider)
         {
-            throw new NotImplementedException();
+            return Convert.ToUInt16(m_value);
         }
 
+        /// <internalonly/>
         int IConvertible.ToInt32(IFormatProvider provider)
         {
-            throw new NotImplementedException();
+            return Convert.ToInt32(m_value);
         }
 
+        /// <internalonly/>
         uint IConvertible.ToUInt32(IFormatProvider provider)
         {
-            throw new NotImplementedException();
+            return m_value;
         }
 
+        /// <internalonly/>
         long IConvertible.ToInt64(IFormatProvider provider)
         {
-            throw new NotImplementedException();
+            return Convert.ToInt64(m_value);
         }
 
+        /// <internalonly/>
         ulong IConvertible.ToUInt64(IFormatProvider provider)
         {
-            throw new NotImplementedException();
+            return Convert.ToUInt64(m_value);
         }
 
+        /// <internalonly/>
         float IConvertible.ToSingle(IFormatProvider provider)
         {
-            throw new NotImplementedException();
+            return Convert.ToSingle(m_value);
         }
 
+        /// <internalonly/>
         double IConvertible.ToDouble(IFormatProvider provider)
         {
-            throw new NotImplementedException();
+            return Convert.ToDouble(m_value);
         }
 
-        decimal IConvertible.ToDecimal(IFormatProvider provider)
+        /// <internalonly/>
+        Decimal IConvertible.ToDecimal(IFormatProvider provider)
         {
-            throw new NotImplementedException();
+            return Convert.ToDecimal(m_value);
         }
 
+        /// <internalonly/>
         DateTime IConvertible.ToDateTime(IFormatProvider provider)
         {
-            throw new NotImplementedException();
-        }
-
-        string IConvertible.ToString(IFormatProvider provider)
-        {
-            throw new NotImplementedException();
+            throw new InvalidCastException(Environment.GetResourceString("InvalidCast_FromTo", "UInt32", "DateTime"));
         }
 
         object IConvertible.ToType(Type conversionType, IFormatProvider provider)
         {
+            //TODO: UInt32 - ToType
             throw new NotImplementedException();
         }
     }
